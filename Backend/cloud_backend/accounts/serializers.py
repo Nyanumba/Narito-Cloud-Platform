bfd1c186cab33f7c1e0e38bbff21f75a911fbd71
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2026-02-26 15:13:53 +0300

    Created API endpoints for user registration and login, created a user model and serializer class for the rest framework

---
+++ Backend/cloud_backend/accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+    
+    class Meta:
+        model = User
+        # required input fields 
+        fields = ['id', 'email', 'username', 'password']
+        
+    def create(self, validated_data):
+            user = User.objects.create_user(
+                email = validated_data['email'],
+                username = validated_data['username'],
+                password = validated_data ['password']
+            )
+            
+            return user
