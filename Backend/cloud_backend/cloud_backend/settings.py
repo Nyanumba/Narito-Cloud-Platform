507c8280ebb153e06b183f80b5438329e79eb35a
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2026-03-03 16:06:20 +0300

    added a new url link to display the email verification page

--- Backend/cloud_backend/cloud_backend/settings.py
+++ Backend/cloud_backend/cloud_backend/settings.py
@@ -132,3 +132,5 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
+#frontend_url
+FRONTEND_URL = os.getenv("FRONTEND_URL", default = 'https://localhost:8000')
