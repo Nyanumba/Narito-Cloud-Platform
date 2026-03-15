50826d6b590336b7227cd3978a43e942b7c98eae
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2026-03-15 15:55:59 +0300

    added the url for email verification

--- Backend/cloud_backend/accounts/urls.py
+++ Backend/cloud_backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, VerifyEmailView, ResendVerificationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name = 'register'),
-    path('login/', TokenObtainPairView.as_view(), name = 'login'),
-    path ('refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
+    path('resend-verification/', ResendVerificationView.as_view(), name='resend-verification'),
 ]
